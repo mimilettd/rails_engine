3ce936bacedcea420875940b831d461eaddd1294
Mike Heft <mikeheft@gmail.com>
2017-10-03 10:31:50 -0600

    fix belongs to relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
-  belongs_to :customer, class_name: 'Customer', foreign_key: 'customer_id'
+  belongs_to :merchant, require: false
+  belongs_to :customer, require: false
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
