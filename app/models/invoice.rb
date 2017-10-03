ec3fb08678d09926bd64a4f7abb22649c25790d2
Mike Heft <mikeheft@gmail.com>
2017-10-03 09:16:14 -0600

    Add relationship for item, and finish up relationship for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,6 @@
 class Invoice < ApplicationRecord
+  belongs_to :customer, class_name: 'Customer', foreign_key: 'customer_id'
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
