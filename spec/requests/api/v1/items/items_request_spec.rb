1ab8813b600b8cc71fd073e64440ddc7ae7d5483
Mike Heft <mikeheft@gmail.com>
2017-10-03 09:04:52 -0600

    Add test for specific item endpoint

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -11,4 +11,14 @@ describe "Items API" do
     expect(items.count).to eq(3)
     expect(items.first["id"]).to eq(i.first.id)
   end
+
+  it "returns specific item" do
+    i = create_list(:item, 3)
+    id = i.first.id
+    get "/api/v1/items/#{id}"
+
+    items = JSON.parse(response.body)
+
+    expect(items["id"]).to eq(id)
+  end
 end
