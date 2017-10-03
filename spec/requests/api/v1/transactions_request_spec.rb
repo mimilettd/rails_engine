62fb49c2a6a05a1321926f0487958cf87c99c9f4
mimilettd <mimilettd@gmail.com>
2017-10-02 18:27:14 -0600

    resolve merge conflict

---
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Transactions API" do
+  it "sends a list of transactions" do
+    create_list(:transaction, 3)
+
+    get '/api/v1/transactions'
+
+    expect(response).to be_success
+
+    # transactions = JSON.parse(response.body)
+  end
+end
