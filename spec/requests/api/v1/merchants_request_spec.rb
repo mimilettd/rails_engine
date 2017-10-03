f62e622a8935836cd89cfec8528464e71dcd48f6
mimilettd <mimilettd@gmail.com>
2017-10-02 20:23:46 -0600

    api can do a single find of merchant by id, name, created_at and updated_at

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -23,4 +23,37 @@ describe "Merchants API" do
     expect(response).to be_success
     expect(merchant["id"]).to eq(id)
   end
+
+  it "can get one merchant by its name" do
+    name = create(:merchant).name
+
+    get "/api/v1/merchants/find?name=#{name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["name"]).to eq(name)
+  end
+
+  it "can get one merchant by its date created at" do
+    factory_merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?created_at=#{factory_merchant.created_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(factory_merchant.id)
+  end
+
+  it "can get one merchant by its date updated at" do
+    factory_merchant = create(:merchant)
+
+    get "/api/v1/merchants/find?updated_at=#{factory_merchant.updated_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(factory_merchant.id)
+  end
 end
