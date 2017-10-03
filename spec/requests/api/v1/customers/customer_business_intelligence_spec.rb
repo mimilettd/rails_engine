fce994045139b7eb6f851beebae0bec0bbebd7f2
Mike Heft <mikeheft@gmail.com>
2017-10-03 15:16:27 -0600

    skip current test I'm working on

--- spec/requests/api/v1/customers/customer_business_intelligence_spec.rb
+++ spec/requests/api/v1/customers/customer_business_intelligence_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  it 'returns #favorite_customer' do
+ xit 'returns #favorite_customer' do
     id = create_list(:customer, 10).first.id
     create_list(:invoice_item, 30)
 
