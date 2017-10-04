2801250d269273e774b51cedc5a2497df24bed40
mimilettd <mimilettd@gmail.com>
2017-10-04 14:06:06 -0600

    refactor total revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,15 +8,13 @@ class Merchant < ApplicationRecord
       .where(id: filter[:id], transactions: {result: 'success'})
       .group(:id)
       .sum("quantity * unit_price")
-      sum = result[filter[:id].to_i] / 100.to_f
-      {"revenue"=>sum.to_s}
     else
       result = joins(invoices: [:invoice_items, :transactions])
       .where(transactions: {result: "success"}, invoices: {created_at: filter[:date]})
       .group(:id)
       .sum("quantity * unit_price")
-      sum = result[filter[:id].to_i] / 100.to_f
-      {"revenue"=>sum.to_s}
     end
+    sum = result[filter[:id].to_i] / 100.to_f
+    {"revenue"=>sum.to_s}
   end
 end
