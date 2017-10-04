f2b05eec61674723dd32c1675090505292b366e9
Mike Heft <mikeheft@gmail.com>
2017-10-04 11:18:33 -0600

    Refactor #top_earner

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 
   def self.top_earners(quantity=nil)
     select('items.*')
-    .joins(:invoice_items)
+    .joins(invoices: :transactions)
+    .merge(Transaction.successful)
     .group(:id)
     .order('SUM(invoice_items.quantity * invoice_items.unit_price) DESC')
     .limit(quantity)
