2974a664f8f1cc6fd6b63b503d822c6517b85c7b
mimilettd <mimilettd@gmail.com>
2017-10-04 20:17:28 -0600

    transaction api can get one transaction by its id

---
+++ spec/requests/api/v1/transactions/transactions_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Transactions API" do
+  it "sends a list of transactions" do
+    create_list(:transaction, 3, result: 'success')
+
+    get '/api/v1/transactions'
+
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+
+    expect(transactions.count).to eq(3)
+  end
+
+  it "can get one transaction by its id" do
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/#{id}"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+
+    expect(transaction["id"]).to eq(id)
+  end
+end
