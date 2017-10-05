cfb73e4ff41b0366be7cfff6c0d9bcb26f410ac1
mimilettd <mimilettd@gmail.com>
2017-10-04 23:06:25 -0600

    refactored Customers::InvoicesController

--- app/controllers/api/v1/customers/invoices_controller.rb
+++ app/controllers/api/v1/customers/invoices_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Customers::InvoicesController < ApplicationController
   def index
-    render json: Invoice.where(customer_id: params[:id])
+    render json: Customer.find(params[:id]).invoices
   end
 end
