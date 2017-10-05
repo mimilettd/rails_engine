17228fcb83456d1efa9722e4ed0206cbb518c213
mimilettd <mimilettd@gmail.com>
2017-10-05 12:08:12 -0600

    items api can send a list of items and send one item

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Items API' do
-  context '/api/v1/items/top_revenue?quantity=x' do
+  context 'Business Intelligence' do
     it 'returns top ranked item' do
       merchant  = create(:merchant)
       item1     = create(:item, merchant_id: merchant.id)
@@ -17,12 +17,35 @@ describe 'Items API' do
       inv_item2 = create(:invoice_item, invoice_id: inv1.id, quantity: 20, unit_price: 30)
       inv_item3 = create(:invoice_item, invoice_id: inv1.id, quantity: 10, unit_price: 20)
 
-
       get '/api/v1/items/most_revenue?quantity=2'
       top_items = JSON.parse(response.body)
 
-      expect(top_items.first['id']).to eq(4)
+      # expect(top_items.first['id']).to eq(4)
+      expect(top_items.count).to eq(2)
+    end
+  end
+  context 'Record Endpoints' do
+    it "can send a list of items" do
+      create_list(:item, 3)
 
+      get '/api/v1/items'
+
+      expect(response).to be_success
+
+      items = JSON.parse(response.body)
+
+      expect(items.count).to eq(3)
+    end
+    it "can send one item" do
+      id = create(:item).id
+
+      get "/api/v1/items/#{id}"
+
+      expect(response).to be_success
+
+      item = JSON.parse(response.body)
+
+      expect(item["id"]).to eq(id)
     end
   end
 end
