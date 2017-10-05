57fe60195b657fde8843f6540d1a684b261f7989
mimilettd <mimilettd@gmail.com>
2017-10-04 20:55:10 -0600

    transactions api can get all transactions by matching result

--- spec/requests/api/v1/transactions/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions/transactions_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Transactions API" do
-  it "sends a list of transactions" do
+  it "can send a list of transactions" do
     create_list(:transaction, 3, result: 'success')
 
     get '/api/v1/transactions'
@@ -25,7 +25,7 @@ describe "Transactions API" do
     expect(transaction["id"]).to eq(id)
   end
 
-  it "find first instance by id" do
+  it "can find first instance by id" do
     id = create_list(:transaction, 3, result: 'success').first.id
     get "/api/v1/transactions/find?id=#{id}"
 
@@ -35,7 +35,7 @@ describe "Transactions API" do
     expect(transaction["id"]).to eq(id)
   end
 
-  it "find first instance by invoice id" do
+  it "can find first instance by invoice id" do
     id = create_list(:transaction, 3, result: 'success').first.invoice_id
 
     get "/api/v1/transactions/find?invoice_id=#{id}"
@@ -46,7 +46,7 @@ describe "Transactions API" do
     expect(transaction["invoice_id"]).to eq(id)
   end
 
-  it "find first instance by credit card number" do
+  it "can find first instance by credit card number" do
     id = create_list(:transaction, 3, result: 'success').first.credit_card_number
 
     get "/api/v1/transactions/find?credit_card_number=#{id}"
@@ -55,5 +55,76 @@ describe "Transactions API" do
 
     expect(response).to be_success
     expect(transaction["credit_card_number"]).to eq(id)
+  end
+
+  it "can find first instance by result" do
+    result = create_list(:transaction, 3).first.result
+
+    get "/api/v1/transactions/find?result=#{result}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["result"]).to eq(result)
+  end
+
+  it "can get all transactions by matching id" do
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/find_all?id=#{id}"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.first["id"]).to eq(id)
+    expect(transactions.count).to eq(1)
+  end
+
+  it "can get all transactions by matching invoice id" do
+    id = create_list(:transaction, 3).first.invoice_id
+
+    get "/api/v1/transactions/find_all?invoice_id=#{id}"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.first["invoice_id"]).to eq(id)
+    expect(transactions.count).to eq(1)
+  end
+
+  it "can get all transactions by matching invoice id" do
+    id = create_list(:transaction, 3).first.invoice_id
+
+    get "/api/v1/transactions/find_all?invoice_id=#{id}"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.first["invoice_id"]).to eq(id)
+    expect(transactions.count).to eq(1)
+  end
+
+  it "can get all transactions by matching credit card number" do
+    credit_card_number = create_list(:transaction, 3).first.credit_card_number
+
+    get "/api/v1/transactions/find_all?credit_card_number=#{credit_card_number}"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.first["credit_card_number"]).to eq(credit_card_number)
+    expect(transactions.count).to eq(3)
+  end
+
+  it "can get all transactions by matching result" do
+    result = create_list(:transaction, 3, result: 'success').first.result
+
+    get "/api/v1/transactions/find_all?result=#{result}"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.first["result"]).to eq(result)
+    expect(transactions.count).to eq(3)
   end
 end
