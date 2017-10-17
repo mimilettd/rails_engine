671a2c91064bb8f2a24dce6b50d21a786e2ef0bc
Mike Heft <mikeheft@gmail.com>
2017-10-17 15:01:02 -0600

    Add swagger-docs, can generate json files, won't show on swaggerui

--- app/controllers/api/v1/customers/favorite_merchant_controller.rb
+++ app/controllers/api/v1/customers/favorite_merchant_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::Customers::FavoriteMerchantController < ApplicationController
+
+  respond_to :json
+
+  swagger_controller :favorite_merchant, 'Favorite Merchant'
+
+  swagger_api :show do
+    summary 'Returns merchant that a certain customer shopped at most.'
+    notes 'Notes...'
+  end
+
   def show
     customer = Customer.find(params[:id])
     render json: customer.favorite_merchant
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
   namespace :api do
     namespace :v1 do
       namespace :merchants do
